8c245e2ae5a39409cf091f1bbe0b967cf554507a
AniketVerma0614 <aniketverma0614@gmail.com>
2025-01-21 08:38:58 +0530

    "Added boilerplate code for ejs-mate layout engine and enabled static CSS file serving for the server."

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+//app.js
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -23,6 +25,9 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+// use ejs-locals for all ejs templates:
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 // Root route
 app.get("/", (req, res) => {
@@ -98,6 +103,15 @@ app.put("/listings/:id", async (req, res) => {
     res.status(500).send("Error updating the listing.");
   }
 });
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
 
 // Start the server
 app.listen(8080, () => {
